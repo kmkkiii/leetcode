af320cc0399fc9d3050dc42a7f057bc0f7e478a1
kmakimaki <rskkmkmk@gmail.com>
2022-06-20 22:37:12 +0900

    1. two-sum submit

--- problems/typescript/1.two-sum.ts
+++ problems/typescript/1.two-sum.ts
@@ -6,7 +6,15 @@
 
 // @lc code=start
 function twoSum(nums: number[], target: number): number[] {
-  
+  // numsをループして、2個ずつ総当たりで取り出して和がtargetと等しいか判定する。
+  for (let i = 0; i < nums.length; i++ ) {
+    for (let j = i + 1; j < nums.length; j++ ) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]; 
+      }
+    }
+  }
+  return [];
 };
 // @lc code=end
 
